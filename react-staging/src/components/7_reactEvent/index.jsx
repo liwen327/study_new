3def18dc4f2a44f4589db19a2f75644a41ca2967
liwz <liwz@yonyou.com>
2024-12-16 16:52:04 +0800

    reactEvent;d面试题

---
+++ react-staging/src/components/7_reactEvent/index.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: liwz
+ * @Date: 2024-11-27 14:04:15
+ * @LastEditors: liwz
+ * @LastEditTime: 2024-12-03 16:33:56
+ * @FilePath: /study_new/react-staging/src/components/7_reactEvent/index.jsx
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
+import React from 'react'
+
+export default function index() {
+  return <div></div>
+}
